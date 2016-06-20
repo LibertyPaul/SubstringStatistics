acf2c1b1847a87c726540157a567c7dd132a0282
LibertyPaul <git@libertypaul.ru>
2016-06-20 13:59:06 +0300

    Added std::tmpnam() error handling

--- Filesystem.hpp
+++ Filesystem.hpp
@@ -8,20 +8,25 @@
 class Filesystem{
 public:
 	static std::string tmpnam(){
-		return std::string(std::tmpnam(nullptr));
+		const char * const result = std::tmpnam(nullptr);
+		if(result == nullptr){
+			throw std::runtime_error("Failed to create tmp file name");
+		}
+
+		return std::string(result);
 	}
 
 	static void remove(const std::string &filename){
 		const int result = std::remove(filename.c_str());
 		if(result != 0){
-			throw std::runtime_error("std::remove error. code: " + std::to_string(result));
+			throw std::runtime_error("std::remove error. code: " + std::to_string(result) + "\nFilename: " + filename);
 		}
 	}
 
 	static void rename(const std::string &from, const std::string &to){
 		const int result = std::rename(from.c_str(), to.c_str());
 		if(result != 0){
-			throw std::runtime_error("std::rename error. code: " + std::to_string(result));
+			throw std::runtime_error("std::rename error. code: " + std::to_string(result) + "\nArguments: from:" + from + " to:" + to);
 		}
 	}
 
